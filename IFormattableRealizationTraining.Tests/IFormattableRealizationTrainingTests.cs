2909eae47e64874f104e1a85fede56c892ab2fdd
Ilya Dvortsevoy <links309@gmail.com>
2018-07-12 00:16:18 +0300

    + Customer class created + unit tests for Customer class added

---
+++ IFormattableRealizationTraining.Tests/IFormattableRealizationTrainingTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace IFormattableRealizationTraining.Tests
+{
+    [TestFixture]
+    public class IFormattableRealizationTrainingTests
+    {
+        [TestCase(
+        "Alex",
+        "+37800000",
+        123567,
+        "NCR",
+        ExpectedResult = "Customer record: Alex, +37800000, 123567")]
+
+        [TestCase(
+        "Dmitry",
+        "+1234235",
+        56476,
+        "N",
+        ExpectedResult = "Customer record: Dmitry")]
+
+        [TestCase(
+        "Vlad",
+        "+436565",
+        111335656,
+        "C",
+        ExpectedResult = "Customer record: +436565")]
+
+        [TestCase(
+        "Vlad",
+        "+436565",
+        111335656,
+        "R",
+        ExpectedResult = "Customer record: 111335656")]
+        public string ToString_Method_returns_expected_result(
+            string name,
+            string contactPhone,
+            int revenue,
+            string format)
+        {
+            decimal decRevenue = Convert.ToDecimal(revenue);
+            var customer = new Customer(name, contactPhone, decRevenue);
+
+            return customer.ToString(format);
+        }
+
+        [TestCase("", "124234", 342433)]
+        [TestCase("Victor", "", 43435)]
+        public void ToString_Method_throws_ArgumentException(
+            string name,
+            string contactPhone,
+            int revenue)
+        {
+            decimal decRevenue = Convert.ToDecimal(revenue);
+
+            Assert.That(
+                () => new Customer(name, contactPhone, decRevenue),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase("Victor", "124234", -33)]
+        [TestCase("Victor", "345647", -43435)]
+        [TestCase("Victor", "124234", -13)]
+        [TestCase("Victor", "345647", -1)]
+        [TestCase("Victor", "345647", -4000000)]
+        public void ToString_Method_throws_ArgumentOutOfRangeException(
+            string name,
+            string contactPhone,
+            int revenue)
+        {
+            decimal decRevenue = Convert.ToDecimal(revenue);
+
+            Assert.That(
+                () => new Customer(name, contactPhone, decRevenue),
+                Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
---
+++ IFormattableRealizationTraining/Customer.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Globalization;
+
+namespace IFormattableRealizationTraining
+{
+    /// <summary>
+    /// Represents model of a customer
+    /// </summary>
+    public class Customer : IFormattable
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IFormattableRealizationTraining.Customer"/> class
+        /// with default parametres
+        /// </summary>
+        /// <exception cref="ArgumentException"> Thrown when name or contactPhone is null or empty </exception>
+        /// <exception cref="ArgumentOutOfRangeException"> Thrown when revenue is less then zero </exception>
+        public Customer()
+            : this("Jeffrey Richter", "+1(425) 555 - 0100", 1000000)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IFormattableRealizationTraining.Customer"/> class
+        /// with specific parametres
+        /// </summary>
+        /// <param name="name"> Name of the customer </param>
+        /// <param name="contactPhone"> Contact phone of the customer </param>
+        /// <param name="revenue"> Revenue of the customer </param>
+        /// <exception cref="ArgumentException"> Thrown when name or contactPhone is null or empty </exception>
+        /// <exception cref="ArgumentOutOfRangeException"> Thrown when revenue is less then zero </exception>
+        public Customer(string name, string contactPhone, decimal revenue)
+        {
+            ValidateArgs(name, contactPhone, revenue);
+
+            Name = name;
+            ContactPhone = contactPhone;
+            Revenue = revenue;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets name of the current customer
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets contact phone of the current customer
+        /// </summary>
+        public string ContactPhone { get; private set; }
+
+        /// <summary>
+        /// Gets revenue of the current customer
+        /// </summary>
+        public decimal Revenue { get; private set; }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Returns string representation of the name, contact phone and revenue
+        /// of the current customer
+        /// </summary>
+        /// <returns> String representation of the customer's info </returns>
+        /// <exception cref="FormatException"> Thrown when format specifier is invalid </exception>
+        public override string ToString()
+        {
+            return ToString("NCR", CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Returns string representation of the current customer's info
+        /// in a specific format
+        /// </summary>
+        /// <param name="format"> Format specifier of string representation:
+        /// N - show only name
+        /// C - show only contact phone
+        /// R - show only revenue
+        /// NC - show name and contact phone
+        /// NR - show name and revenue
+        /// CR - show contact phone and revenue
+        /// NCR - show name, contact phone and revenue
+        /// </param>
+        /// <returns> String representation of the customer's info </returns>
+        /// <exception cref="FormatException"> Thrown when format specifier is invalid </exception>
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        /// <summary>
+        /// Returns string representation of the current customer's info
+        /// in a specific format using specific format provider
+        /// </summary>
+        /// <param name="format">Format specifier of string representation:
+        /// N - show only name
+        /// C - show only contact phone
+        /// R - show only revenue
+        /// NC - show name and contact phone
+        /// NR - show name and revenue
+        /// CR - show contact phone and revenue
+        /// NCR - show name, contact phone and revenue
+        /// </param>
+        /// <param name="provider"> Specific format provider </param>
+        /// <returns> String representation of the customer's info </returns>
+        /// <exception cref="FormatException"> Thrown when format specifier is invalid </exception>
+        public string ToString(string format, IFormatProvider provider)
+        {
+            if (string.IsNullOrEmpty(format))
+            {
+                format = "NCR";
+            }
+
+            if (provider == null)
+            {
+                provider = CultureInfo.CurrentCulture;
+            }
+
+            switch (format.ToUpperInvariant())
+            {
+                // Show only name
+                case "N":
+                    return string.Format(provider, "Customer record: {0}", Name);
+
+                // Show only contact phone
+                case "C":
+                    return string.Format(provider, "Customer record: {0}", ContactPhone);
+
+                // Show only revenue
+                case "R":
+                    return string.Format(provider, "Customer record: {0}", Revenue);
+
+                // Show name and contact phone
+                case "NC":
+                    return string.Format(provider, "Customer record: {0}, {1}", Name, ContactPhone);
+
+                // Show name and revenue
+                case "NR":
+                    return string.Format(provider, "Customer record: {0}, {1}", Name, Revenue);
+
+                // Show contact phone and revenue
+                case "CR":
+                    return string.Format(provider, "Customer record: {0}, {1}", ContactPhone, Revenue);
+
+                // Show name, contact phone and revenue
+                case "NCR":
+                    return string.Format(provider, "Customer record: {0}, {1}, {2}", Name, ContactPhone, Revenue);
+                default:
+                    throw new FormatException("Invalid string format");
+            }
+        }
+        #endregion
+
+        #region Private Helper Methods
+        private void ValidateArgs(
+            string name, 
+            string contactPhone, 
+            decimal revenue)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(nameof(name));
+            }
+            else if (string.IsNullOrEmpty(contactPhone))
+            {
+                throw new ArgumentException(nameof(contactPhone));
+            }
+            else if (revenue < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(revenue));
+            }
+        }
+        #endregion
+    }
+}
