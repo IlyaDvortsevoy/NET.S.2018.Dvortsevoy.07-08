ff7cdb27d3fb804f002abd6afbbd948f042decb4
Ilya Dvortsevoy <links309@gmail.com>
2018-07-13 00:38:06 +0300

    + CustomerDetailedFormatter class realized + unit tests for CustomerDetailedFormatter class added

---
+++ IFormatProviderRealization.Tests/IFormatProviderRealizationTests.cs
@@ -0,0 +1,16 @@
+﻿using IFormattableRealizationTraining;
+using NUnit.Framework;
+
+namespace IFormatProviderRealization.Tests
+{
+    [TestFixture]
+    public class IFormatProviderRealizationTests
+    {
+        [Test, TestCaseSource(typeof(Data), "TestCases")]
+        public string Format_Method_of_CustomerDetailedFormatter_class_returns_proper_result(
+                Customer customer)
+        {
+            return string.Format(new CustomerDetailedFormatter(), "{0}", customer);
+        }
+    }
+}
